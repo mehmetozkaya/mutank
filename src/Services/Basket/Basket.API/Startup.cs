a13fdbd4d94af5c1d982c4c1fc44bc8cd4e3ee8b
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2021-02-07 14:28:49 +0300

    basket

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -29,7 +29,7 @@ namespace Basket.API
         {
             services.AddStackExchangeRedisCache(options =>
             {
-                options.Configuration = Configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+                options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
             
             services.AddScoped<IBasketRepository, BasketRepository>();
