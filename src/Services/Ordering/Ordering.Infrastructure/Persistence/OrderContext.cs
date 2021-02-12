a7921b78319651c2f66f4a0f17932d7d700418ee
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2021-02-12 18:50:40 +0300

    OrderRepository

--- src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
 using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Ordering.Infrastructure.Persistence
 {
@@ -10,5 +14,24 @@ namespace Ordering.Infrastructure.Persistence
         }
 
         public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "swn";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "swn";
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
