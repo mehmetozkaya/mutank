dcf0ad298571282f3dbe08ed3978c04f58774351
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2021-02-22 18:10:03 +0300

    elastic

--- src/BuildingBlocks/Common.Logging/SeriLogger.cs
+++ src/BuildingBlocks/Common.Logging/SeriLogger.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Sinks.Elasticsearch;
 using System;
@@ -10,13 +11,15 @@ namespace Common.Logging
         public static Action<HostBuilderContext, LoggerConfiguration> Configure =>
            (context, configuration) =>
            {
+               var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
+
                configuration
                     .Enrich.FromLogContext()
                     .Enrich.WithMachineName()
                     .WriteTo.Debug()
                     .WriteTo.Console()
                     .WriteTo.Elasticsearch(
-                        new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+                        new ElasticsearchSinkOptions(new Uri(elasticUri))
                         {
                             IndexFormat = $"applogs-{context.HostingEnvironment.ApplicationName?.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
                             AutoRegisterTemplate = true,
